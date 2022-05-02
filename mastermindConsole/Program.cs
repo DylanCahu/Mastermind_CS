18b3677d537972a30b01ae2d52bd80be43bdfb6c
Mars Cahu <dydypot@gmail.com>
2022-05-02 16:23:07 +0200

    scan debug

--- mastermindConsole/Program.cs
+++ mastermindConsole/Program.cs
@@ -13,19 +13,18 @@
     public class Module
     {
 
+        public static int[] choixPC = {0,0,0,0};
 
-        public static int[] choixPC = new int[4];
-
-        public static int[] choixJoueur = new int[4];
+        public static int[] choixJoueur = {0,0,0,0};
 
         public static int genererRandom()
-        {//permet de générer un chiffre aléatoire entre 1 et 4
+        {//permet de générer un chiffre aléatoire entre 1 et 6
             Random rd = new Random();
-            int rand_num = rd.Next(1, 4);
+            int rand_num = rd.Next(1, 7);
             return rand_num;
         }
         
-        public void choixOrdinateur()
+        public static void choixOrdinateur()
         {//génère un tableau de 4 chiffres aléatoires
             for (int i = 0; i < 4; i++)
             {
@@ -34,11 +33,15 @@
         }
 
 
-        public Array ChoixJoueur()
+        public static int[] ChoixJoueur()
         {//transforme un int de 4 chiffre en tableau de ces 4 même chiffres
+
+            Console.WriteLine("Veuillez taper chiffres entre 1 et 6"); 
             
             //variables locale
             int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n);
+
             int choixvalide = 0;
             for (int i = 0; i < 4; i++)
             {
@@ -47,13 +50,15 @@
             }
             for (int i = 0; i < 4; i++)
             {
-                if (0 < choixJoueur[i] && choixJoueur[i] < 5)
+                if (0 < choixJoueur[i] && choixJoueur[i] < 7)
                 {
                     choixvalide = choixvalide + 1;
                 }
             }
             if (choixvalide == 4 && choixJoueur.Length == 4)
             {
+                Console.WriteLine("Vous avez choisi : " + PrintValues(choixJoueur));
+                Console.WriteLine("Et l'ordi a choisi : " + PrintValues(choixPC));
                 return choixJoueur;
             }
             else
@@ -66,46 +71,62 @@
 
 
         public static int nbCommun(int[] TabPC, int[] TabJoueur)
-        {
+        {//pour chaque chiffre du tableau, si il est dans le tableau du joueur, on incrémente le compteur
             //variables locale
             int cpt = 0;
-            int[] choixPC2 = TabPC;
+            int[] choixPC2 = { 0, 0, 0, 0 };
+            int[] choixJoueur2 = { 0, 0, 0, 0 };
+            TabPC.CopyTo(choixPC2, 0);
+            TabJoueur.CopyTo(choixJoueur2, 0);
             for (int i = 0; i < 4; i++)
             {
-                if (choixPC2[i] == TabJoueur[i])
+                if (choixPC2[i] == choixJoueur2[i])
                 {
-                    choixPC2[i] = 6;
+                    choixPC2[i] = -1;
+                    choixJoueur2[i] = -2; //Pour supprimer les doublons
                     cpt = cpt + 1;
                 }
             }
             if (cpt == 4)
             {
-                Console.WriteLine("Bravo ! Tu as réussi. La solution été : ", TabPC);
+                Console.WriteLine("Bravo ! Tu as réussi. La solution été : "+ PrintValues(TabPC));
+                Console.ReadLine();
             }
             else if (cpt > 1)
             {
-                Console.WriteLine(cpt + "bonnes réponses, essaye encore !");
+                Console.WriteLine(cpt + " bonnes réponses, essaye encore !");
             }
             else
             {
-                Console.WriteLine(cpt + "bonne réponse, essaye encore !");
+                Console.WriteLine(cpt + " bonne réponse, essaye encore !");
             }
             for (int i = 0; i < 4; i++)
             {
-                if (choixPC2[i] != TabJoueur[i] && TabJoueur.count(choixPC2[i]) > 0)
+
+                if (choixPC2[i] == choixJoueur2[0] || choixPC2[i] == TabJoueur[1] || choixPC2[i] == TabJoueur[2] || choixPC2[i] == TabJoueur[3])
                 {
-                    Console.WriteLine("Mais", choixPC2[i], "est correct");
+                    Console.WriteLine("Mais " + choixJoueur2[i] + " est mal placé");
                 }
             }
             return cpt;
         }
+        
+        public static string PrintValues(int[] myArr)
+        {
+            string result = null;
+            foreach (int i in myArr)
+            {
+                result = result + i;
+            }
+            return result;
+        }
 
-        public void Jeu()
+    public static void Jeu()
         {
             //variables locale
             var nbJeu = 0;
             choixOrdinateur();
-            Console.WriteLine(choixPC); //debug
+            Console.WriteLine(PrintValues(choixPC)); //debug
             ChoixJoueur(); //1ère manche
             while (nbCommun(choixPC, choixJoueur) != 4)
             {
@@ -117,12 +138,12 @@
                 }
                 else
                 {
-                    Console.WriteLine("PERDUUUUUU, c'était : ", choixPC);
+                    Console.WriteLine("Perdu, c'était : "+ PrintValues(choixPC));
                 }
             }
         }
         
-        public void main()
+        static void Main()
         {
             Jeu();
         }
